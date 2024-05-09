609451d830babed26026984907371d63e81e991b
kkimjuseong <rofodzl1@naver.com>
2024-05-09 14:08:45 +0900

    sql연습

---
+++ sql문제풀이.sql
@@ -0,0 +1,42 @@
+
+
+CREATE TABLE 绊按_38 (
+    绊按ID NUMBER PRIMARY KEY,
+    绊按疙 VARCHAR2(100)
+);
+
+CREATE TABLE 牧刨明_38 (
+    牧刨明ID NUMBER PRIMARY KEY,
+    牧刨明疙 VARCHAR2(100)
+);
+
+CREATE TABLE 眠玫牧刨明_38 (
+    绊按ID NUMBER,
+    牧刨明ID NUMBER,
+    眠玫措惑老磊 DATE
+);
+
+CREATE TABLE 厚急龋牧刨明_38 (
+    绊按ID NUMBER,
+    牧刨明ID NUMBER,
+    殿废老磊 DATE
+);
+
+
+INSERT INTO 绊按_38 VALUES (1, '辫枚荐');
+INSERT INTO 绊按_38 VALUES (2, '冠康锐');
+COMMIT;
+
+
+INSERT INTO 牧刨明_38 VALUES (1, '康拳');
+INSERT INTO 牧刨明_38 VALUES (2, '局聪皋捞记');
+INSERT INTO 牧刨明_38 VALUES (3, '促钮膏磐府');
+
+INSERT INTO 眠玫牧刨明_38 VALUES (1, 1, SYSDATE);
+INSERT INTO 眠玫牧刨明_38 VALUES (1, 2, SYSDATE);
+INSERT INTO 眠玫牧刨明_38 VALUES (2, 2, SYSDATE);
+
+INSERT INTO 厚急龋牧刨明_38 VALUES (1, 2, SYSDATE);
+INSERT INTO 厚急龋牧刨明_38 VALUES (2, 1, SYSDATE);
+INSERT INTO 厚急龋牧刨明_38 VALUES (2, 3, SYSDATE);
+
